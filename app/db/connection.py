65e8d931acb2da71a8640997782fc5da3bab3b3a
Łukasz Sobkowiak <sobkowiakluke@gmail.com>
2026-02-26 10:47:52 +0100

    Błędy po refactorze

---
+++ app/db/connection.py
@@ -0,0 +1,19 @@
+from app.settings import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+import mysql.connector
+
+class Database:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+
+    def cursor(self, **kwargs):
+        kwargs["dictionary"] = True
+        return self.conn.cursor(**kwargs)
+
+
+    def commit(self):
+        self.conn.commit()
